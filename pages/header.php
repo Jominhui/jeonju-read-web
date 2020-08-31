d93b8b0cb9c33b654442b64ec2ec54fc51fe4bc3
Jominhui <rihito2021@naver.com>
2020-08-31 17:28:34 +0900

    [Add] overview page

--- pages/header.php
+++ pages/header.php
@@ -3,11 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script src="./assets/js/jquery-3.4.1.min.js"></script>
-    <script src="./assets/js/bootstrap.min.js"></script>
-    <link rel="stylesheet" href="./assets/css/bootstrap.min.css">
-    <link rel="stylesheet" href="./assets/fontawesome/css/font-awesome.min.css">
-    <link rel="stylesheet" href="./assets/css/index.css">
+    <script src="../assets/js/jquery-3.4.1.min.js"></script>
+    <script src="../assets/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="../assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../assets/fontawesome/css/font-awesome.min.css">
+    <link rel="stylesheet" href="../assets/css/index.css">
     <title>전주 독서 대전</title>
 </head>
 <body>
@@ -20,9 +20,9 @@
                     <div class="sub-menu-area">
                         <div class="sub-menu-title">전주독서대전</div>
                         <hr class="sub-menu-line">
-                        <div class="sub-menu">대전 소개</div>
+                        <a href="../index.php"><div class="sub-menu">대전 소개</div></a>
                         <div class="sub-menu">로그인 및 회원가입</div>
-                        <div class="sub-menu">행사개요</div>
+                        <a href="../pages/overview.php"><div class="sub-menu">행사개요</div></a>
                     </div>
                 </div>
                 <div class="main-menu">온라인 책만들기</div>
