bc7da52c0b14ffe508fee90d5dcc8bbdb5a612c7
Jominhui <rihito2021@naver.com>
2020-08-31 17:04:05 +0900

    [Update] main page(php)

---
+++ pages/footer.php
@@ -0,0 +1,13 @@
+<footer>
+            <div class="logo" style="margin: 0"></div>
+            <div id="copyright">(54819) 전주시 덕진구 솔내로 212 (송천동2가 168-5) | 전화 : 063-281-6535 | 팩스 : 063-281-5286<br>
+                <span>COPYRIGHT 2020 전주독서대전. ALL RIGHT RESERVED.</span></div>
+            <div id="icon_list">
+                <div class="icon"><i class="fa fa-facebook-official"></i></div>
+                <div class="icon"><i class="fa fa-twitter"></i></div>
+                <div class="icon"><i class="fa fa-instagram"></i></div>
+            </div>
+        </footer>
+    </div>
+</body>
+</html>
---
+++ pages/header.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="./assets/js/jquery-3.4.1.min.js"></script>
+    <script src="./assets/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="./assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="./assets/fontawesome/css/font-awesome.min.css">
+    <link rel="stylesheet" href="./assets/css/index.css">
+    <title>전주 독서 대전</title>
+</head>
+<body>
+    <div id="wrap">
+        <header>
+            <div class="logo"></div>
+            <div class="main-menu-area">
+                <div class="main-menu">
+                    전주독서대전
+                    <div class="sub-menu-area">
+                        <div class="sub-menu-title">전주독서대전</div>
+                        <hr class="sub-menu-line">
+                        <div class="sub-menu">대전 소개</div>
+                        <div class="sub-menu">로그인 및 회원가입</div>
+                        <div class="sub-menu">행사개요</div>
+                    </div>
+                </div>
+                <div class="main-menu">온라인 책만들기</div>
+                <div class="main-menu">
+                    독자와의 만남
+                    <div class="sub-menu-area">
+                        <div class="sub-menu-title">독자와의 만남</div>
+                        <hr class="sub-menu-line">
+                        <div class="sub-menu">예약하기</div>
+                        <div class="sub-menu">예약확인</div>
+                    </div>
+                </div>
+                <div class="main-menu">
+                    관리자페이지
+                    <div class="sub-menu-area">
+                        <div class="sub-menu-title">관리자페이지</div>
+                        <hr class="sub-menu-line">
+                        <div class="sub-menu">행사등록</div>
+                        <div class="sub-menu">예약관리</div>
+                        <div class="sub-menu">예약현황</div>
+                    </div>
+                </div>
+            </div>
+        </header>
