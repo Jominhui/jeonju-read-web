a46ac646c483f090f3e0262cbd994d9db52954f6
Jominhui <rihito2021@naver.com>
2020-09-02 11:21:39 +0900

    [Add] CRUD

--- lib/server.php
+++ lib/server.php
@@ -26,6 +26,89 @@ switch ($action){
             alert($username, "님 회원가입이 완료되었습니다.");
         }
         break;
+
+    case 'insert' :
+        $sql = "
+        INSERT INTO meet SET
+        Title = ? ,
+        Photo = ? ,
+        Writer = ? ,
+        Target = ? ,
+        ReleaseDate = ?,        
+        MDate = ? ,
+        MTime = ? ;
+        ";
+        $Photo = "";
+        if( isset($_FILES['Photo']) ) {
+            $up_folder = "../uploads/";
+            $photo_file = $_FILES['Photo']['name'];
+            $tmp_file = $_FILES['Photo']['tmp_name'];
+            $target = $up_folder . $photo_file;
+            if( move_uploaded_file($tmp_file, $target) ) {
+                $Photo = $photo_file;
+            }
+        }
+        $execArr = [$Title, $Photo, $Writer, $Target, $ReleaseDate, $MDate, $MTime];
+        $url = "../pages/adminlist.php";
+        break;
+
+    case 'update' :
+        $Photo = "";
+        if( isset($_FILES['Photo']) ) {
+            $up_folder = "../uploads/";
+            $photo_file = $_FILES['Photo']['name'];
+            $tmp_file = $_FILES['Photo']['tmp_name'];
+            $target = $up_folder . $photo_file;
+            if( move_uploaded_file($tmp_file, $target) ) {
+                $Photo = $photo_file;
+            }
+        }
+        $sql = "
+        UPDATE meet SET
+        Title = ? ,";
+        if( $Photo ) $sql .= "Photo = ? ,";
+        $sql .= "Writer = ? ,
+        Target = ? ,
+        ReleaseDate = ?,        
+        MDate = ? ,
+        MTime = ? 
+        WHERE idx = ?                                                                                                                                                                                                                          
+        ";
+
+        $url = "../pages/adminlist.php";
+        if($Photo) $execArr = [$Title, $Photo, $Writer, $Target, $ReleaseDate, $MDate, $MTime, $idx];
+        else $execArr = [$Title, $Writer, $Target, $ReleaseDate, $MDate, $MTime, $idx];
+        break;
+
+    case 'delete' :
+        $sql = "DELETE FROM meet WHERE idx = ?";
+        $execArr = [$idx];
+        $url = "../pages/adminlist.php";
+        break;
+
+    case 'reservate' :
+        $sql = "
+        INSERT INTO reservate SET
+        title = ? ,
+        photo = ? ,
+        writer = ? ,
+        username = ? ,
+        gender = ? ,
+        age = ? ,
+        school = ? ,
+        date = ? ,
+        about = ? , 
+        ready = ?;
+        ";
+        $execArr = [$title, $photo, $writer, $username, $gender, $age, $school, $date, $about, $ready];
+        $url = "../index.php";
+        break;
+        
+    case 'res-delete' :
+        $sql = "DELETE FROM reservate WHERE idx = ?";
+        $execArr = [$idx];
+        $url = "/pages/reservate_my_list.php";
+        break;
 }
 $res = query($sql, $execArr);
 move($url);
